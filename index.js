4191e210f050c93030f4574798ba610888264bea
dipanc1 <91887201+dipancnextgen@users.noreply.github.com>
2022-03-16 20:27:24 +0530

    sending static page

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+    // res.send('hello world')
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+})
