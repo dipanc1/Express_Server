a54182078abc905e43e722bccc6890271d845af8
dipanc1 <91887201+dipancnextgen@users.noreply.github.com>
2022-03-17 14:51:14 +0530

    used template engine -handlebars used it

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
 
 // app.use(express.static(path.join(__dirname, 'static')))
-app.use('/', require(path.join(__dirname, './routes/blog')))
+app.use('/', require(path.join(__dirname, './routes/blog')));
 
 app.listen(3000, () => {
-    console.log(`Blog app listening on port http://localhost:3000`)
-})
+    console.log(`Blog app listening on port http://localhost:3000`);
+});
--- routes/blog.js
+++ routes/blog.js
@@ -1,25 +1,31 @@
 const express = require('express')
 const path = require('path')
+const { title } = require('process')
 const router = express.Router()
 const blogs = require('../data/blogs')
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../templates/index.html'))
+    // res.sendFile(path.join(__dirname, '../templates/index.html'))
+    res.render('home')
 })
 
 router.get('/blog', (req, res) => {
-    // blogs.forEach(blog => {
-    //     console.log(blog.title)
-    // })
-    res.sendFile(path.join(__dirname, '../templates/blogHome.html'))
+    res.render('blogHome', {
+            blogs: blogs
+        })
+        // res.sendFile(path.join(__dirname, '../templates/blogHome.html'))
 })
 
 router.get('/blogpost/:slug', (req, res) => {
     myBlog = blogs.filter((e) => {
-            return e.slug == req.params.slug
+        return e.slug == req.params.slug
+    })
+    res.render('blogPage', {
+            title: myBlog[0].title,
+            content: myBlog[0].content
         })
         // console.log(myBlog)
-    res.sendFile(path.join(__dirname, '../templates/blogPage.html'))
+        // res.sendFile(path.join(__dirname, '../templates/blogPage.html'))
 })
 
 module.exports = router
