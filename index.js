6cc8fa87e264221a3c149ac1c43899555802c849
dipanc1 <91887201+dipancnextgen@users.noreply.github.com>
2022-03-17 13:09:37 +0530

    now i will turn it into blog website ( not me :''()

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-    // res.send('hello world')
-    res.sendFile(__dirname + '/index.html')
+app.get('/about/:name', (req, res) => {
+    res.send('hello world ' + req.params.name)
+        // res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(3000, () => {
