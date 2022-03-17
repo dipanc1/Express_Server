00725f773dab394c8e3999634e4a360f30b90db5
dipanc1 <91887201+dipancnextgen@users.noreply.github.com>
2022-03-17 13:37:31 +0530

    started making blog website

---
+++ data/blogs.js
@@ -0,0 +1,8 @@
+blogs = [{
+    id: 1,
+    title: 'Блог 1',
+    content: 'Описание блога 1',
+    slug: 'blog-1',
+}]
+
+module.exports = blogs;
--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')))
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/about/:name', (req, res) => {
-    res.send('hello world ' + req.params.name)
-        // res.sendFile(__dirname + '/index.html')
-})
+// app.use(express.static(path.join(__dirname, 'static')))
+app.use('/', require(path.join(__dirname, './routes/blog')))
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+    console.log(`Blog app listening on port http://localhost:3000`)
 })
